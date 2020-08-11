7de1509e9155f4058d9868974dcc89d1c856d28c
Axel Obscura <axelobscura@Axels-MacBook-Pro.local>
2020-08-11 10:51:45 -0500

    First commit

---
+++ pages/quiz.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+function quiz(){
+    return(
+        <main className={styles.main}>
+            <h1>CUESTIONARIO IMCYC</h1>
+        </main>
+    )
+}
+
+export default quiz;
